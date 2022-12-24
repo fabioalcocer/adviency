cd322659bbfe3c509100723729ad67036f18e16c
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-12-23 22:40:43 -0400

    add tailwind

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,12 @@
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
   return (
-    <main className={inter.className}>
-      <div className={styles.container}>
-        <h1 className={styles.title__page}>Regalos</h1>
+    <main className={`${inter.className} flex items-center min-h-screen bg-emerald-800`}>
+      <div className='bg-white text-slate-900 mx-auto max-w-sm p-4'>
+        <h1 className='text-3xl text-center'>Regalos</h1>
         <div>
           <ul>
             <li>Tenis</li>
