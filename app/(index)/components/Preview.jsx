8c154ca68cbd8144e611b895583552992cf2f2a8
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-01-16 13:52:51 -0400

    add react-audio-player library

--- app/(index)/components/Preview.jsx
+++ app/(index)/components/Preview.jsx
@@ -18,7 +18,7 @@ function Preview ({ gifts }) {
           <div className='w-[90%] mx-auto justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
             <div className='relative w-auto my-6 mx-auto max-w-3xl'>
               <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
-                <div className='flex items-start flex-col justify-between gap-4 p-6 py-4 rounded-t'>
+                <div className='flex items-start flex-col justify-between gap-4 p-5 py-4 rounded-t'>
                   <h3 className='text-2xl font-semibold'>Preview</h3>
                   {gifts.length
                     ? (
